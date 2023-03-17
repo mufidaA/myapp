b8eed9c43cb74935d27e5d4ab367b785d34e300d
mufidaA <mufidaalakulju@gmail.com>
2023-03-17 13:47:44 +0200

    Login Ui done

--- app/src/main/java/com/example/myapp/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/myapp/ui/screens/LoginScreen.kt
@@ -1,15 +1,23 @@
 package com.example.myapp.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import android.graphics.fonts.FontFamily
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapp.ui.navigation.common.DefaultButton
 import com.example.myapp.ui.theme.MyappTheme
@@ -18,17 +26,51 @@ import com.example.myapp.ui.theme.MyappTheme
 fun LoginScreen(
     navigateToHome: () -> Unit
 ) {
-    Column (
+    Column(
         modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally) {
-        Text("Login Screen", fontSize = 40.sp)
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        val username = remember { mutableStateOf(TextFieldValue()) }
+        val password = remember { mutableStateOf(TextFieldValue()) }
+        Text("Login", fontSize = 40.sp)
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Username") },
+            value = username.value,
+            onValueChange = { username.value = it })
 
-        DefaultButton(
-            text = "Log In",
-            onClick = navigateToHome
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password") },
+            value = password.value,
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            onValueChange = { password.value = it })
+
+        Spacer(modifier = Modifier.height(20.dp))
+        Box(modifier = Modifier.padding(40.dp, 0.dp, 40.dp, 0.dp)) {
+            Button(
+                onClick = navigateToHome ,
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+            ) {
+                Text(text = "Login")
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+        ClickableText(
+            text = AnnotatedString("Forgot password?"),
+            onClick = { },
+            style = TextStyle(
+                fontSize = 14.sp,
+            )
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
